407704ab879d30498db52331a3269554c0c62e44
XGhin <gabrieljdalposso@gmail.com>
2023-08-31 22:19:53 -0300

    terminando ex18

--- Linguagem de Programação/Trabalho para o dia 13.09.2023/ex18.cpp
+++ Linguagem de Programação/Trabalho para o dia 13.09.2023/ex18.cpp
@@ -3,26 +3,32 @@
     que são primos e suas respectivas posições no vetor.
 */
 
-int primos[10], A[10], i;
+#include <stdio.h>
+
+int primos[10], A[10], i, p;
 
 int main(){
     for(i = 0; i < 10; i++){
         printf("Digite o numero na posicao %d: ",i);
         scanf("%d", &A[i]);
-        for(i = 2; i * i <= A[i]; i++){
+
             if(A[i] <= 0){
-                //pass
+                continue;
             }
-            else if(A[i]%i == 0){
-            //pass
+
+        for(p = 2; p * p <= A[i]; p++){
+            if(A[i]%p == 0){
+            break;
             }
-            else{
-                primos[i] = A[i];
-            } 
+        }    
+        if(p * p > A[i]){
+            primos[i] = A[i];
+        } 
         }
-    }
 
     for(i = 0; i < 10; i++){
-        printf("{%d}\n", primos[i]);
+        if(primos[i] != 0){
+            printf("{%d}\n", primos[i]);
+        }
     }
 }
