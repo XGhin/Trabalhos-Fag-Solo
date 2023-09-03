7cb6b36823fba5433d2ed0bb8b95ee285ef2b66f
XGhin <gabrieljdalposso@gmail.com>
2023-09-03 13:33:30 -0300

    ex 16 e fazendo ex15

---
+++ Linguagem de Programação/Trabalho para o dia 13.09.2023/ex15.cpp
@@ -0,0 +1,11 @@
+/*
+15 – Preencha um vetor A do tipo inteiro de 5 posições. Em um segundo vetor B do tipo
+booleano com 5 posições colocar em cada posição TRUE caso o valor do número do índice
+correspondente no vetor A for PAR, caso contrário colocar FALSE;
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int A[5] = {2, 5, 7, 8, 9}, i = 0;
+bool B[5];
---
+++ Linguagem de Programação/Trabalho para o dia 13.09.2023/ex16.cpp
@@ -0,0 +1,25 @@
+/*
+16 – Preencha 2 vetores A e B de 5 posições cada com números reais. Em um vetor C de 10
+posições preencher com valores de A e B intercalados.
+*/
+
+#include <stdio.h>
+
+int A[5] = {5, 3, 2, 6, 9}, B[5] = {1, 5, 7, 2, 8}, C[10], i = 0, r = 0, z = 0;
+
+
+int main(){
+    for(i = 0; i < 10; i += 2){
+        C[i] = A[r];
+        r++;
+    }
+    
+    for(i = 1; i < 10; i += 2){
+        C[i] = B[z];
+        z++;
+    }
+
+    for(i = 0; i < 10; i++){
+        printf("{%d}", C[i]);
+    }
+}
